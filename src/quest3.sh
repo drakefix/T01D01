f98d72d9b1789c87fb3bada15cc64257a32d9a9a
drakefix <drakefix@student.21-school.ru>
2022-03-01 03:33:13 +0300

    test commit

---
+++ src/quest3.sh
@@ -0,0 +1,8 @@
+mv door_managment_fi door_managment_files
+cd door_managment_files
+mkdir door_confiquration
+mv *.conf door_confiquration
+mkdir door_logs
+mv *.log door_logs
+mkdir door_map
+mv door_map_1.1 door_map
